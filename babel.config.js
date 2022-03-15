485996a711cc8dbac99855284b415e39f78edc91
周泽飞 <zefei.zhou@tongdun.net>
2022-03-15 16:35:41 +0800

    fix: 🐛 ceshi

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = {
       {
         "libraryName": "antd",
         "libraryDirectory": "es",
-        "style": true // `style: true` 会加载 less 文件
+        "style": true
       },
       "antd"
     ],
